f82317bf6da71d41733378fbf2e845ab2c44bfb9
JoonChicken <jbsquirrel06@gmail.com>
2025-08-08 23:59:06 -0700

    Grid and meterstick opacity changes

--- world/ground_plane.gd
+++ world/ground_plane.gd
@@ -5,7 +5,7 @@ var current_mode := modes.VIEWMODE
 
 func change_viewmode(new_mode: int) -> void:
     if current_mode != modes.PLAYMODE && new_mode == modes.PLAYMODE:
-        hide()
+        mesh.material.shader_paramter.opacity = 0.5
     elif current_mode == modes.PLAYMODE && new_mode != modes.PLAYMODE:
-        show()
+        mesh.material.shader_paramter.opacity = 1.0
     current_mode = new_mode
