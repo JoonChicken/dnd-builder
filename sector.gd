328912e07205631c86c36e2072aaec80bf1147d5
JoonChicken <jbsquirrel06@gmail.com>
2025-08-12 16:33:43 -0700

    Sector mesh generation improvements

--- sector.gd
+++ sector.gd
@@ -1,21 +1,36 @@
 extends Node3D
 
+@export var origin := Vector3.ZERO
 @export_group("Sector Shape")
 @export var base_vertices : Array
 @export var floor_height := 0.0
 @export var ceil_height := 2.0
+@export_group("Sector Components")
+@export var mesh_node : MeshInstance3D
+@export var collider_node : CollisionShape3D
 @export_group("Sector Appearance")
-@export var flip_normals := true
+@export var hide_sector_from_players := false
 @export var wall_material : Material
 @export var floor_material : Material
 
-@onready var mesh_node : MeshInstance3D = $Mesh
 var st := SurfaceTool.new()
     
 
 func _ready() -> void:
     regenerate()
     
+
+func generate_origin() -> void:
+    # start with finding a centerpoint for triangle fan; try the average of vertices and midpoints
+    # (should work well enough with convex-ish shapes)
+    var sum = Vector2.ZERO # find average point of all the vertices
+    for i in range(base_vertices.size()):
+        sum += base_vertices[i]
+        var midpoint : Vector2 = base_vertices[i] + (base_vertices[(i + 1) % base_vertices.size()] - base_vertices[i]) / 2
+        sum += midpoint
+    var avg : Vector2 = sum / (base_vertices.size() * 2.0)
+    origin = Vector3(avg.x, (floor_height + ceil_height) / 2, avg.y)
+
 
 func regenerate() -> void:
     # first clear any collision shapes from last generation
@@ -60,16 +75,12 @@ func regenerate() -> void:
             st.add_vertex(Vector3(vertex.x, ceil_height, vertex.y)) # top right of quad
 
     # generate floor and ceiling
-    # start with finding a centerpoint for triangle fan; try the average first (should work well with convex shapes)
-    var sum = Vector2.ZERO # find average point of all the vertices
-    for i in range(base_vertices.size()):
-        sum += Vector2(base_vertices[i].x, base_vertices[i].y)
-    var centerpoint : Vector2 = sum / base_vertices.size()
+    # first find origin of shape
+    generate_origin()
+    var centerpoint := Vector2(origin.x, origin.z)
     # check if centerpoint is within the polygon made with the base vertices
-    if Geometry2D.is_point_in_polygon(centerpoint, base_vertices):
-        print("successfully found midpoint")
-    else:
-        print("uhhh mesh might be weird")
+    if not Geometry2D.is_point_in_polygon(centerpoint, base_vertices):
+        print("MESH WARNING: Midpoint not generated correctly. Try to split up convex shapes into concave components.")
     
     # generate triangle fan meshes for floor and ceiling
     var vertices_floor := [Vector3(centerpoint.x, floor_height, centerpoint.y)]
@@ -80,15 +91,18 @@ func regenerate() -> void:
         vertices_floor.append(Vector3(bottom_vertex.x, floor_height, bottom_vertex.y))
         vertices_ceil.append(Vector3(top_vertex.x, ceil_height, top_vertex.y))
     st.add_triangle_fan(vertices_floor)
-    st.add_triangle_fan(vertices_ceil)
+    st.add_triangle_fan(vertices_ceil) # handle uvs here somehow
 
     # other annoying stuff
     st.index()
     st.generate_normals()
     var mesh = st.commit()
     mesh_node.mesh = mesh
-    mesh_node.create_multiple_convex_collisions()
+    collider_node.shape = mesh.create_trimesh_shape()
 
 
-func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
-    pass # Replace with function body.
+func _on_input_event(_camera: Node, event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
+    if event is InputEventMouseButton:
+        if event.pressed and event.button_index == MouseButton.MOUSE_BUTTON_LEFT:
+            print("clicked!!")
+            mesh_node.set_layer_mask_value(6, not mesh_node.get_layer_mask_value(6))
