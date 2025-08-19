a251baaa032e0247b54cff139aeb2bb4eefe44f4
JoonChicken <jbsquirrel06@gmail.com>
2025-08-19 13:13:16 -0700

    Sector mesh creation complete

--- main.gd
+++ main.gd
@@ -2,6 +2,8 @@ extends Node3D
 
 @export_group("World")
 @export var vertex_preview : MeshInstance3D
+@export_subgroup("Scenes to instance")
+@export var sector_scene : PackedScene
 @export_group("Control")
 @export var topmenu : Panel
 @export var sidemenu : Panel
@@ -14,11 +16,12 @@ extends Node3D
 enum modes {VIEWMODE, EDITMODE, PLAYMODE}
 var current_mode := modes.VIEWMODE
 
-enum tools {SELECT, NEW_SECTOR}
+enum tools {SELECT, SECTOR}
 var current_tool := tools.SELECT
 
 var sectors : Array
 var selected_sector : Area3D
+var sector_being_created : Area3D
 
 var snap : bool
 var mouse_global_coords : Vector3
@@ -26,6 +29,7 @@ var mouse_global_coords : Vector3
 
 func _ready() -> void:
     selected_sector = null
+    sector_being_created = null
     snap = false
     vertex_preview.hide()
     sidemenu.hide()
@@ -35,7 +39,7 @@ func _ready() -> void:
 func _process(_delta: float) -> void:    
     if Input.is_action_just_pressed("change_viewmode"):
         change_viewmode()
-    if current_mode == modes.EDITMODE and current_tool == tools.NEW_SECTOR:
+    if current_mode == modes.EDITMODE and current_tool == tools.SECTOR:
         if Input.is_action_just_pressed("snapping"):
             snap = true
         elif Input.is_action_just_released("snapping"):
@@ -48,15 +52,23 @@ func _process(_delta: float) -> void:
             
 
 func _unhandled_input(event: InputEvent) -> void:
-    if current_mode == modes.EDITMODE and current_tool == tools.NEW_SECTOR:
+    if current_mode == modes.EDITMODE and current_tool == tools.SECTOR:
         if event is InputEventMouseButton:
             if event.pressed and event.button_index == MouseButton.MOUSE_BUTTON_LEFT:
+                if sector_being_created == null:
+                    sector_being_created = sector_scene.instantiate()
+                    add_child(sector_being_created)
+                    sector_being_created.clicked.connect(_sector_selected) # see if this works after finishing custom mesh generation
+                sector_being_created.push_vert3(mouse_global_coords)
                 var test_mesh := MeshInstance3D.new()
                 test_mesh.mesh = BoxMesh.new()
                 test_mesh.scale = Vector3(0.1, 0.1, 0.1)
                 test_mesh.set_name("test")
                 test_mesh.position = mouse_global_coords
                 add_child(test_mesh)
+        elif event is InputEvent:
+            if event.is_action_pressed("escape") and sector_being_created != null:
+                sector_being_created = null 
 
 
 func change_viewmode() -> void:
@@ -68,14 +80,28 @@ func change_viewmode() -> void:
         sidemenu.hide()
         current_tool_label.hide()
         current_mode_label.text = "VIEW MODE"
+        vertex_preview.hide()
     elif current_mode == modes.EDITMODE:
         sidemenu.show()
         current_tool_label.show()
         current_mode_label.text = "EDIT MODE"
+        if current_tool == tools.SECTOR:
+            vertex_preview.show()
+        else:
+            vertex_preview.hide()
     else: # modes.PLAYMODE
         sidemenu.hide()
         current_tool_label.hide()
         current_mode_label.text = "PLAY MODE"
+        vertex_preview.hide()
+
+
+func _sector_selected(ref) -> void:
+    print("_sector_selected called")
+    if selected_sector != null:
+        selected_sector.deselect()
+    selected_sector = ref
+    selected_sector.select()
 
 
 func _change_tool_to_select() -> void:
@@ -85,6 +111,6 @@ func _change_tool_to_select() -> void:
 
 
 func _change_tool_to_new_sector() -> void:
-    current_tool = tools.NEW_SECTOR
+    current_tool = tools.SECTOR
     vertex_preview.show()
     current_tool_label.text = "SECTOR TOOL"
--- sector.gd
+++ sector.gd
@@ -1,23 +1,50 @@
 extends Node3D
 
+signal clicked(ref)
+
 @export var origin := Vector3.ZERO
 @export_group("Sector Shape")
 @export var base_vertices : Array
 @export var floor_height := 0.0
+@export var floor_height_fudge := 0.001
 @export var ceil_height := 2.0
 @export_group("Sector Components")
 @export var mesh_node : MeshInstance3D
 @export var collider_node : CollisionShape3D
 @export_group("Sector Appearance")
+@export var selected := false
 @export var hide_sector_from_players := false
 @export var wall_material : Material
 @export var floor_material : Material
 
 var st := SurfaceTool.new()
-    
+var corrected_vertices : Array
 
-func _ready() -> void:
-    regenerate()
+
+func get_num_verts() -> int:
+    return base_vertices.size()
+
+
+func push_vert2(vert : Vector2) -> void:
+    base_vertices.append(vert)
+    if base_vertices.size() > 2:
+        regenerate()
+    
+    
+func push_vert3(vert : Vector3) -> void:
+    base_vertices.append(Vector2(vert.x, vert.z))
+    if base_vertices.size() > 2:
+        regenerate()
+    
+    
+func pop_vertex() -> Vector2:
+    return base_vertices.pop_back()
+    if base_vertices.size() > 2:
+        regenerate()
+    else:
+        clear_visual()
+        if base_vertices.size() == 0:
+            queue_free()
     
 
 func generate_origin() -> void:
@@ -32,26 +59,35 @@ func generate_origin() -> void:
     origin = Vector3(avg.x, (floor_height + ceil_height) / 2, avg.y)
 
 
-func regenerate() -> void:
-    # first clear any collision shapes from last generation
+func clear_visual() -> void:
+    clear_collision()
+    mesh_node.mesh = null
+
+
+func clear_collision() -> void:
     var children := mesh_node.get_children()
     for child in children:
         child.queue_free()
-        
+
+
+func regenerate() -> void:
+    # first clear any collision shapes from last generation
+    clear_collision()
+    
     # fix wrong side being culled
     # if vertices are arranged clockwise, then it's all good, but if not, the face has to be flippped by
     # going backward through the array of vertices
-    # first get midpoint of line between first two vertices; direction vector will be important later
+    # first copy the vertices array
+    corrected_vertices = base_vertices.duplicate()
+    # get midpoint of line between first two vertices; direction vector will be important later
     var edge : Vector2 = base_vertices[1] - base_vertices[0] # MAKE SURE base_vertices.size() > 2
     var midpoint : Vector2 = base_vertices[0] + edge / 2
     var edge_offset : Vector2 = edge.normalized() * 0.01
     var is_right_inner : bool = Geometry2D.is_point_in_polygon(midpoint + edge_offset.rotated(PI/2), base_vertices)
     var is_left_inner : bool = Geometry2D.is_point_in_polygon(midpoint + edge_offset.rotated(-PI/2), base_vertices)
-    if is_right_inner and not is_left_inner:
-        print("yay")
-    elif is_left_inner and not is_right_inner:
-        base_vertices.reverse()
-    else:
+    if is_left_inner and not is_right_inner:
+        corrected_vertices.reverse()
+    elif not(not is_left_inner and is_right_inner):
         print("Something's wrong with your points. Please make it look less bad.")
     
     # SurfaceTool setup
@@ -60,17 +96,17 @@ func regenerate() -> void:
 
     # generate walls
     var next_tri_end : Vector3
-    for i in range(base_vertices.size() + 1):
-        var vertex : Vector2 = base_vertices[i % base_vertices.size()]
+    for i in range(corrected_vertices.size() + 1):
+        var vertex : Vector2 = corrected_vertices[i % corrected_vertices.size()]
         # think about tris as going from right to left (clockwise in mesh)
         if i != 0:
             var duplicate_corner := Vector3(vertex.x, ceil_height, vertex.y) # top left of quad
             st.add_vertex(duplicate_corner) # finish off last tri
             st.add_vertex(duplicate_corner) # start new tri
-            st.add_vertex(Vector3(vertex.x, floor_height, vertex.y)) # bottom left of quad
+            st.add_vertex(Vector3(vertex.x, floor_height + floor_height_fudge, vertex.y)) # bottom left of quad
             st.add_vertex(next_tri_end)
-        if i != base_vertices.size():
-            next_tri_end = Vector3(vertex.x, floor_height, vertex.y) # bottom right of quad
+        if i != corrected_vertices.size():
+            next_tri_end = Vector3(vertex.x, floor_height + floor_height_fudge, vertex.y) # bottom right of quad
             st.add_vertex(next_tri_end)
             st.add_vertex(Vector3(vertex.x, ceil_height, vertex.y)) # top right of quad
 
@@ -79,16 +115,16 @@ func regenerate() -> void:
     generate_origin()
     var centerpoint := Vector2(origin.x, origin.z)
     # check if centerpoint is within the polygon made with the base vertices
-    if not Geometry2D.is_point_in_polygon(centerpoint, base_vertices):
+    if not Geometry2D.is_point_in_polygon(centerpoint, corrected_vertices):
         print("MESH WARNING: Midpoint not generated correctly. Try to split up convex shapes into concave components.")
     
     # generate triangle fan meshes for floor and ceiling
-    var vertices_floor := [Vector3(centerpoint.x, floor_height, centerpoint.y)]
+    var vertices_floor := [Vector3(centerpoint.x, floor_height + floor_height_fudge, centerpoint.y)]
     var vertices_ceil := [Vector3(centerpoint.x, ceil_height, centerpoint.y)]
-    for i in range(base_vertices.size() + 1):
-        var bottom_vertex : Vector2 = base_vertices[i % base_vertices.size()]
-        var top_vertex : Vector2 = base_vertices[(base_vertices.size() - i) % base_vertices.size()]
-        vertices_floor.append(Vector3(bottom_vertex.x, floor_height, bottom_vertex.y))
+    for i in range(corrected_vertices.size() + 1):
+        var bottom_vertex : Vector2 = corrected_vertices[i % corrected_vertices.size()]
+        var top_vertex : Vector2 = corrected_vertices[(corrected_vertices.size() - i) % corrected_vertices.size()]
+        vertices_floor.append(Vector3(bottom_vertex.x, floor_height + floor_height_fudge, bottom_vertex.y))
         vertices_ceil.append(Vector3(top_vertex.x, ceil_height, top_vertex.y))
     st.add_triangle_fan(vertices_floor)
     st.add_triangle_fan(vertices_ceil) # handle uvs here somehow
@@ -99,10 +135,20 @@ func regenerate() -> void:
     var mesh = st.commit()
     mesh_node.mesh = mesh
     collider_node.shape = mesh.create_trimesh_shape()
+    
+    
+func select() -> void:
+    selected = true
+    mesh_node.set_layer_mask_value(6, true)
 
+
+func deselect() -> void:
+    selected = false
+    mesh_node.set_layer_mask_value(6, false)
+    
 
 func _on_input_event(_camera: Node, event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
     if event is InputEventMouseButton:
         if event.pressed and event.button_index == MouseButton.MOUSE_BUTTON_LEFT:
-            print("clicked!!")
-            mesh_node.set_layer_mask_value(6, not mesh_node.get_layer_mask_value(6))
+            print("sector click detected")
+            clicked.emit(self)
